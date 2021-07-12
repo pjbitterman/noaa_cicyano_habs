4ff8582a2d369742be73905626fc39418839ebbd
Patrick Bitterman <patrick.bitterman@gmail.com>
2021-07-12 10:57:51 -0500

    updated for the chesapeake noaa dowload page

--- src/noaa_download_script.R
+++ src/noaa_download_script.R
@@ -2,13 +2,19 @@ library(tidyverse)
 library(downloader)
 library(rvest)
 
+
+# URLs on the NOAA HABS Explorer page
 champ.url <- "https://products.coastalscience.noaa.gov/habs_explorer/index.php?path=djFYWEE0NURTMllpTkN5VUVzdmtIcW5YWE83TTdDRzg2VldJWTFoc3JQdnBpckZ1K2FyTHgzUjUxSFNlOWlVZw==&uri=VWtuM1UzbVNVN0RsZzJMeTJvNlNpM29OalF0WTFQQjVZVnpuS3o5bnh1Ym0vYWhtWEh4ck1hREVUamE4SDZ0M0JnSnNMaHk4U2YyaTc0R04zM2ZId0hBbkVjOGkrTnJJMXR3WmtJbGJFeUkyUTY2OCtjUmpSUEFyWXpNWEIzZlc=&type=bllEUXA3TmhSK21RVDlqbFYxMmEwdz09"
 erie.url <- "https://products.coastalscience.noaa.gov/habs_explorer/index.php?path=eXhJRlJpQjQ0NnNyYmQwL21vSFpjWWxoeWtud2l3YmhYSlhBUjM4bTM2QT0=&uri=VWtuM1UzbVNVN0RsZzJMeTJvNlNpM29OalF0WTFQQjVZVnpuS3o5bnh1Ym0vYWhtWEh4ck1hREVUamE4SDZ0M0JnSnNMaHk4U2YyaTc0R04zM2ZId04wdC8rNVdEdEdKTS9CQ3pEY2I0ZDg9&type=bllEUXA3TmhSK21RVDlqbFYxMmEwdz09"
+ches.url <- "https://products.coastalscience.noaa.gov/habs_explorer/index.php?path=bVoraGxra3BOd0pQdnhnL21jd0QxL1hGRjE5QWRQaTVxWVI5VTlPaDlKbz0=&uri=VWtuM1UzbVNVN0RsZzJMeTJvNlNpM29OalF0WTFQQjVZVnpuS3o5bnh1Ym0vYWhtWEh4ck1hREVUamE4SDZ0M0JnSnNMaHk4U2YyaTc0R04zM2ZId0tDRGEwSjVPOVVQSjhjWXF4Z0g1Y0QxQlNaTGVHWHpKVXYxRnlNNnQ1bXA=&type=bllEUXA3TmhSK21RVDlqbFYxMmEwdz09"
 
+# local directories to download to
 champ.dltarget <- fs::fs_path("./downloads/champ")
 erie.dltarget <- fs::fs_path("./downloads/erie")
+ches.dltarget <- fs::fs_path("./downloads/chesapeake")
 
-
+# function to batch download and sort downloaded images
+# automatically skips previously downloaded files
 image_dl_and_org <- function(noaaUrl, downloadUrl, lines_to_skip){
  
   hab.html <- read_html(noaaUrl)
@@ -28,14 +34,14 @@ image_dl_and_org <- function(noaaUrl, downloadUrl, lines_to_skip){
     html_text() 
   
   # the parsing of text has both readmes and a "names" header, so clean up
-  # (get rid of first 3) - this is true for both Erie and Champlain
+  # (get rid of first n, where n = 'lines_to_skip' parameter)
   names.cleaned <- names.all %>%
     tail(length(.) - 3) %>% as.character()
   
   # put them in a data.frame and use regex to extract 
   # the category (e.g., CI, CIcyano) of image
   df <- data.frame(links.cleaned, names.cleaned) %>%
-    mutate(img.type = stringr::str_match(names.cleaned, "_[0-9]\\.(.*?)\\.tif")[,2])
+    mutate(img.type = stringr::str_match(names.cleaned, "(_|-)[0-9]\\.(.*?)\\.tif")[,3])
   
   i.cats <- df$img.type %>% unique()
   
@@ -85,4 +91,4 @@ image_dl_and_org <- function(noaaUrl, downloadUrl, lines_to_skip){
 ### download them ----
 image_dl_and_org(champ.url, champ.dltarget, lines_to_skip = 4) 
 image_dl_and_org(erie.url, erie.dltarget, lines_to_skip = 3)
-
+image_dl_and_org(ches.url, ches.dltarget, lines_to_skip = 3)
